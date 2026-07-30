57491c4a26fde8693fe404747a2a5c7d1cc4fe05
Preet <patilpreetviia@gmail.com>
2026-07-30 09:23:03 +0530

    Use favicon.png as browser tab icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -27,8 +27,8 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
-      { rel: "shortcut icon", type: "image/png", href: "/favicon.png" },
+      { rel: "icon", type: "image/png", href: "/favicon.png" },
+      { rel: "shortcut icon", href: "/favicon.png" },
     ],
   }),
   shellComponent: RootShell,
